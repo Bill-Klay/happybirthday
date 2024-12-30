e29266567bcc6305a79a6f0b30014f7a7c103df0
Bill-Klay <bilal.khan2998@gmail.com>
2024-12-30 20:34:46 +0500

    update for Zoha

--- script/main.js
+++ script/main.js
@@ -1,13 +1,13 @@
 // trigger to play music in the background with sweetalert
 window.addEventListener('load', () => {
     Swal.fire({
-        title: 'Do you want to play music in the background?',
+        title: "Due to modern browser security I'll have to ask your permission to play music",
         icon: 'warning',
         showCancelButton: true,
         confirmButtonColor: '#3085d6',
         cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes',
-        cancelButtonText: 'No',
+        confirmButtonText: 'Yes, please',
+        cancelButtonText: "No, I'm good"
     }).then((result) => {
         if (result.isConfirmed) {
             document.querySelector('.song').play();
